8f4a82503f6c64bca37484b7b016eece3233aee8
Nathan Hwang <thenoviceoof@gmail.com>
2011-12-10 16:10:07 -0500

    Final choosepoint interface

--- ChoosePoint.m
+++ ChoosePoint.m
@@ -2,25 +2,16 @@ function [ color ] = ChoosePoint( img )
 %ChosePoint( image )
 %   Takes an image, prompt user to choose point, return color of point
 
-% init an empty color
-color = [0,0,0];
+f = figure();
+image(img);
 
-f = figure;
-i = image(img);
+p = round(ginput(1));
+disp(p);
 
-    function [] = HandleMouse(src, event)
-        disp('mu');
-    end
+color = img(p(1),p(2),:);
+color = [color(1), color(2), color(3)];
 
-set(f, 'WindowButtonDownFcn', @HandleMouse);
-
-disp('aoeu');
-
-% wait for a mouse click
-k = waitforbuttonpress;
-while k ~= 0
-    k = waitforbuttonpress;
-end
+close(f);
 
 end
 
