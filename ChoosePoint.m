b24c1097bc19baea38539079799a544ac046ec0f
Nathan Hwang <thenoviceoof@gmail.com>
2011-12-10 14:16:14 -0500

    Scaffolding for point chooser and reaction/main

---
+++ ChoosePoint.m
@@ -0,0 +1,10 @@
+function [ color ] = ChoosePoint( image )
+%ChosePoint( image )
+%   Takes an image, prompt user to choose point, return color of point
+
+% init an empty color
+color = [0,0,0];
+
+
+end
+
