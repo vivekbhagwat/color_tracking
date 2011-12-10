69619101750f6043941a90729760a550a6d9c016
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-12-10 17:52:04 -0500

    fixed ReactToBlob

--- ReactToBlob.m
+++ ReactToBlob.m
@@ -2,23 +2,28 @@ function [ react ] = ReactToBlob( serPort, dx, darea)
 % dpos = number between (-1.0, 1.0), 
 
 angle_gain = 1.0;
-foward_speed = 0.1;
+forward_speed = 0.1;
 
 scale_threshold = 0.5;
+pos_threshold = 0.1;
 
 % amount of turn
 angle = angle_gain*dx;
 % use log: 0->infty, 1.0->0.0
-radius = (angle/abs(angle))*(-log(abs(angle)));
+% radius = (angle/abs(angle))*(-log(abs(angle)));
 
 % whether to move forward or back
 react = 1;
 if darea > 1.0 + scale_threshold
-    setFwdVelRadiusRoomba(serPort, forward_speed, radius);
+%     setFwdVelRadiusRoomba(serPort, forward_speed, radius);
+    SetFwdVelAngVelCreate(serPort, -forward_speed, sign(dx)*pi/6);
 elseif darea < 1.0 - scale_threshold
-    setFwdVelRadiusRoomba(serPort, -forward_speed, radius);
+    SetFwdVelAngVelCreate(serPort, forward_speed, sign(dx)*pi/6);
+elseif abs(dx) > pos_threshold
+    SetFwdVelAngVelCreate(serPort, 0, sign(dx) * pi/6);
 else
     react = 0;
+    SetFwdVelAngVelCreate(serPort, 0, 0);
 end
 
 end
