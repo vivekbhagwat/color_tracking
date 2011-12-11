ce9a395ad38fd74b2ce06e7a2972b4c8f9384000
Nathan Hwang <thenoviceoof@gmail.com>
2011-12-10 22:55:48 -0500

    bad indentation

--- main.m
+++ main.m
@@ -10,19 +10,19 @@ center = [center(2), center(1)];
 
 while (1)
     try
-    % find the largest blob of the given color
-    img = GetImage();
-    [position, new_area] = find_largest_blob(img, color);
-    %if new_area < 0
-    %    ReactToBlob(serPort, 0.0, 1.0);
-    %    continue
-    %end
+        % find the largest blob of the given color
+        img = GetImage();
+        [position, new_area] = find_largest_blob(img, color);
+        %if new_area < 0
+        %    ReactToBlob(serPort, 0.0, 1.0);
+        %    continue
+        %end
 
-    % we only care about the differences
-    dpos = (position - center)/center(1);
-    darea = new_area/area;
-    disp([dpos(1), darea]);
-    ReactToBlob(serPort, dpos(1), darea);
+        % we only care about the differences
+        dpos = (position - center)/center(1);
+        darea = new_area/area;
+        disp([dpos(1), darea]);
+        ReactToBlob(serPort, dpos(1), darea);
     catch err
         disp(err);
         % stop the robot, prevent robot chases
